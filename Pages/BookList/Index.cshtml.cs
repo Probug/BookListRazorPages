a0b104809d4908182cd379c7b3a93b9aa9399b9d
Caleb Igwe <igwecalebkene@gmail.com>
2020-03-12 20:11:36 +0100

    third commit

---
+++ Pages/BookList/Index.cshtml.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Services;
+using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookListRazor.Pages.BookList
+{
+    public class IndexModel : PageModel
+    {
+        private readonly BookServices _db;
+        public IndexModel(BookServices db)
+        {
+            _db = db;
+        }
+
+        public IEnumerable<Book> Books {get; set;}
+        public void OnGet()
+        {
+            Books = _db.Get().ToList();
+        }
+    }
+}
